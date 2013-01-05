cb6d31013f3a0425132cc80fe400fc9a5099dc4c
Glow <jiangbian66@gmail.com>
2013-01-05 20:59:48 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+FavClip
+=============
+
+FavClip是从豆瓣网自动分析热门相册并同步保存到人人网账号的自动程序。
+
+License
+=============
+Ruby on Rails is released under the [MIT License](http://www.opensource.org/licenses/MIT).
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Front to the favclip application. This file doesn't do anything, but loads
+ * the require php file, enjoy coding.
+ *
+ * @author Glowin
+ */
+
+define('TEMP', "TEMP");
+
+?>
