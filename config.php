04f068d5a23b45697ba0cf74ba07dfd09f23bb21
Glow Chiang <jiangbian66@gmail.com>
2013-01-14 00:17:42 +0800

    add config.php file

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * 配置数据库
+ */
+define('DB_HOST', 'localhost:3306');
+define('DB_USER', 'root');
+define('DB_PWD', 'li0621');
+define('DB_NAME','favclip');
+define('DOUBAN', 'http://www.douban.com/');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+?>
