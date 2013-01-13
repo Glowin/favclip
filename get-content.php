8fb5f11a63b2a700b7c1960e76dcacf946661564
Glow Chiang <jiangbian66@gmail.com>
2013-01-14 00:18:36 +0800

    insert the user information

---
+++ get-content.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * @author Glowin
+ * @copyright 2013
+ */
+
+set_time_limit(0);
+ini_set('display_errors', 'On');
+require_once("include/simple_html_dom.php");
+
+//根据url获得用户user_name，用户的唯一标识是"user_name"
+function get_user_name( $url ) {
+    if ( $url == '') return 0;
+    $replace = array('http://www.douban.com/people/' => '','/' => '');
+    $user_name = (string) strtr($url, $replace);
+    return $user_name;
+}
+
+//根据url获取相册ID
+function get_alum( $url ) {
+    if ( $url == '') return 0;
+    $replace = array('http://www.douban.com/photos/album/' => '', '/' => '' );
+    $id = (int) strtr($url, $replace);
+    return $id;
+}
+
+//根据url获取相片ID
+function get_photo( $url ) {
+    if ( $url == '') return 0;
+    $replace = array('http://www.douban.com/photos/photo/' => '', '/' => '' );
+    $id = (int) strtr($url, $replace);
+    return $id;
+}
+
+//根据url获得说说ID
+function get_shuoshuo( $url ) {
+    if ( $url == '') return 0;
+    $replace = array('http://www.douban.com/people/' => '');
+    $id = (int) strtr($url, $replace);
+    return $id;
+}
+
+//根据用户user_name获取用户首页HTML
+function html_user_name( $user_name ) {
+    $url = 'http://www.douban.com/people/'. $user_name. '/';
+    //设置 UA 信息
+    ini_set('user_agent', 'Opera/9.80 (Android 2.3.4; Linux; Opera Mobi/build-1107180945; U; en-GB) Presto/2.8.149 Version/11.10');
+    $html = file_get_html( $url );
+
+    return $html;
+}
+
+//上传用户信息到数据库
+function put_userinfo( $html ) {
+    include_once('config.php');
+    $user_name = $html -> find('.infobox .pl', 0) -> find('text', 0);
+    $user_name = trim($user_name);
+    $user_nickname = $html -> find('#content .info h1', 0) -> find('text', 0);
+    $user_nickname = trim($user_nickname);
+    $user_motto = $html -> find('#content .pl', 0) -> find('text', 0);
+    //替换备注中的括号
+    $user_motto = strtr($user_motto, array('(' => '', ')' =>'' ));
+    //获取加入时间
+    $user_joinedtime = $html -> find('.user-info .pl', 0) -> plaintext;
+    $user_joinedtime = explode(' ',$user_joinedtime);
+    $user_joinedtime = strtr($user_joinedtime[2], array('加入' =>''));
+   // return $user_joinedtime;
+    //用户状态默认为1，开启。如果为0则为加入黑名单
+    $user_status = 1;
+    //获得用户ID，通过用户首页的头像图片的命名得来
+    $user_imgurl = $html -> find('.infobox img', 0) -> src;
+    $user_imgurlinfo = explode('/', $user_imgurl);
+    $user_id = (int)strtr($user_imgurlinfo[4], array('ul' => '', '.jpg' => ''));
+
+    //获取用户添加的附加信息
+    $user_intro = $html -> find('.infobox .user-intro', 0) -> innertext;
+    //用户级别默认为0，最高为10
+    $user_level = 0;
+    //关注人数
+    $user_follow = $html -> find('#friend .pl a', 0) -> find('text', 0);
+    $user_follow = (int)strtr($user_follow, array('全部' =>''));
+    $user_follow = trim($user_follow);
+    //被关注人数
+    $user_followed = $html -> find('#friend a', -1) -> plaintext;
+    $user_followed = strtr($user_followed, array(' 被' => '', '人关注' => ''));
+    $user_followed = strrev((int)strrev($user_followed));
+
+    //update the user information to database
+    //上传信息到数据库
+    $insert = 'INSERT INTO user (
+    user_status,
+    user_id,
+    user_name,
+    user_nickname,
+    user_motto,
+    user_intro,
+    user_follow,
+    user_followed,
+    user_joinedtime,
+    user_level
+    ) VALUES (
+    \''.$user_status.'\',
+    \''.$user_id.'\',
+    \''.$user_name.'\',
+    \''.$user_nickname.'\',
+    \''.$user_motto.'\',
+    \''.$user_intro.'\',
+    \''.$user_follow.'\',
+    \''.$user_followed.'\',
+    \''.$user_joinedtime.'\',
+    \''.$user_level.'\'
+    )';
+
+    //连接数据库
+    $db = mysql_connect(DB_HOST, DB_USER, DB_PWD);
+    mysql_select_db(DB_NAME,$db);//
+    //mysql_query('SET NAMES \'gbk\'');
+    //mysql_query('SET CHARACTER_SET \'gbk\'');
+
+    if(! mysql_query($insert, $db) ) {
+        echo '<h1>error!</h1>';
+        echo mysql_error().'<br />';
+        echo 'the sql is '.$insert."<br />";
+    }
+}
+
+echo put_userinfo(html_user_name('Google.de'));
+
+?>
