3c415afc2f31791ac8ec5c949434c8821f49112d
Glow Chiang <jiangbian66@gmail.com>
2013-01-14 00:17:19 +0800

    modify the index.php

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ FavClip
 
 FavClip是从豆瓣网自动分析热门相册并同步保存到人人网账号的自动程序。
 
+使用的类库
+-------------
+[PHP Simple HTML DOM Parser](http://simplehtmldom.sourceforge.net/)   
+>A HTML DOM parser written in PHP5+ let you manipulate HTML in a very easy way!
+
 License
-=============
+-------------
 FavClip is released under the [MIT License](http://www.opensource.org/licenses/MIT).
--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
+
 /**
- * Front to the favclip application. This file doesn't do anything, but loads
- * the require php file, enjoy coding.
- *
  * @author Glowin
+ * @copyright 2013
  */
 
-define('TEMP', "TEMP");
 
 ?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo "FavClip Homepage" ?></title>
+</head>
+<body>
+	<h1>Welcome to FavClip</h1>
+	<p>Favclip 能将豆瓣上的热门相册、热门说说自动保存并发送到您的人人网。</p>
+</html>
