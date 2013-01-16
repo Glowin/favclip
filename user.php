247096a31e3c656d948f9c1f15d6ce9534451646
Glow Chiang <jiangbian66@gmail.com>
2013-01-16 23:21:30 +0800

    添加用户控制页面

---
+++ user.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>用户管理页面</title>
+</head>
+<body>
+<form action="user.php">
+	<label for="url">用户地址：</label>
+	<input type="text" name="url">
+</form>	
+</body>
+</html>
