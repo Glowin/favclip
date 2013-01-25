1783319c5b982c7dce0bda9f9df417b55867bb49
Glow Chiang <jiangbian66@gmail.com>
2013-01-25 17:27:03 +0800

    修改数据库连接文件

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 //============================================================+
 // File name   : tce_db_dal_mysql.php
-// Begin       : 2003-10-12
+// Begin       : 2013-01-16
 //
 // Description : 
 //               
@@ -16,10 +16,11 @@
 // License: MIT License
 //============================================================+
 
+require_once('db_dal.php'); // Database Abstraction Layer for selected DATABASE type
+
 if(!$db = @db_connect(DB_HOST, DB_PORT, DB_USER, DB_PWD, DB_NAME)) {
 	die('<h2>'.db_error().'</h2>');
 }
-require_once('db_dal.php'); // Database Abstraction Layer for selected DATABASE type
 
 
 //============================================================+
