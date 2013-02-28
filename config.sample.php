18adb0b3bddd42e8a2a2f542f57a8d8f1e4dcd6c
Glow Chiang <jiangbian66@gmail.com>
2013-03-01 03:19:53 +0800

    添加数据库默认配置文件

--- config.sample.php
+++ config.sample.php
@@ -5,7 +5,7 @@
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_USER', 'root');
-define('DB_PWD', 'li0621');
+define('DB_PWD', 'password');
 define('DB_NAME','favclip');
 define('DOUBAN', 'http://www.douban.com/');
 define('DB_CHARSET', 'utf8');
